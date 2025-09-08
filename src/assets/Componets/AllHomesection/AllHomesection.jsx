d3f2122b235e25587207d7ffe843b8a95cfd9867
girnarhitesh <hiteshgirnar99@gmail.com>
2025-09-08 18:21:20 +0530

    code change

--- src/assets/Componets/AllHomesection/AllHomesection.jsx
+++ src/assets/Componets/AllHomesection/AllHomesection.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Hero from './Hero/Hero'
-import Services from './Services/Services';
+import Services from './Services/Services'
 import OurClients from './OurClients/OurClients';
 import OurProcess from './OurProcess/OurProcess';
 import Testimonials from './Testimonials/Testimonials';
